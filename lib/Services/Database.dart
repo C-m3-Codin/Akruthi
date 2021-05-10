f9eb2609252133204225cf6d97a600f6dad97f13
cp <cyril199897@gmail.com>
2021-05-11 04:13:51 +0530

    data pull carousel

--- lib/Services/Database.dart
+++ lib/Services/Database.dart
@@ -1,5 +1,6 @@
 import 'package:akruthi/DataModels/StreamEvents.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:convert';
 
 class DatabaseServices {
   FirebaseFirestore db = FirebaseFirestore.instance;
@@ -17,11 +18,33 @@ class DatabaseServices {
 
   geStreamEvents() {
     FirebaseFirestore db = FirebaseFirestore.instance;
-
+    List<StreamingEvents> data = [];
     db.collection("StreamEvents").get().then((querySnapshot) {
       querySnapshot.docs.forEach((result) {
-        print(result.data());
+        // data.add(StreamingEvents.fromJson(json.decode(result.data())));
+        data.add(
+            StreamingEvents.fromJson(json.decode(result.data().toString())));
       });
     });
+    return data;
   }
+}
+
+Future<List<StreamingEvents>> postByPostPath(String postPath) async {
+  List<StreamingEvents> postFromFirebase = [];
+  print(postPath);
+  print("\n\n");
+  await FirebaseFirestore.instance
+      .collection("StreamEvents")
+      .get()
+      .then((querysnapshot) {
+    querysnapshot.docs.forEach((element) {
+      // print(element.data()['title']);
+      postFromFirebase.add(
+          StreamingEvents.fromJson(json.decode(json.encode(element.data()))));
+    });
+  });
+
+  // print('retrieved: ${postFromFirebase.eventName}');
+  return postFromFirebase;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:akruthi/DataModels/StreamEvents.dart';
 import 'package:akruthi/NotificationTry.dart';
 import 'package:akruthi/Services/Database.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'dart:convert';
 // import 'package:overlay_support/overlay_support.dart';
 
 // FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
+double width;
+double height;
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
@@ -55,11 +61,18 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<StreamingEvents> list;
+  List<StreamingEvents> list = [];
+  QuerySnapshot querySnapshot;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+      postByPostPath("asd").then((value) {
+        list = value;
+        setState(() {});
+      });
+    });
 
     var initializationSettingsAndroid =
         AndroidInitializationSettings('@mipmap/ic_launcher');
@@ -91,22 +104,91 @@ class _MyAppState extends State<MyApp> {
 
     getToken();
     // DatabaseServices ab = new DatabaseServices();
-    DatabaseServices ab = new DatabaseServices();
-    ab.geStreamEvents();
+    // DatabaseServices ab = new DatabaseServices();
+    // list = geStreamEvents();
 
     // print("\n\n\n\n\nEachEvent ${list[0].eventName}");
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("bam"),
-      ),
-      body: Center(
-        child: Text("asd"),
-      ),
-    );
+    width = MediaQuery.of(context).size.width;
+    height = MediaQuery.of(context).size.height;
+
+    return list.isEmpty
+        ? Center(
+            child: CircularProgressIndicator(),
+          )
+        : SafeArea(
+            child: Scaffold(
+
+                // appBar: AppBar(
+                //   title: Text("bam"),
+                // ),
+                body: Column(
+              children: [
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   crossAxisAlignment: CrossAxisAlignment.center,
+                //   children: [
+                //     Container(
+                //       color: Colors.blue[400],
+                //       height: 200,
+                //       width: width * .6,
+                //       // child: Text(list[0].eventName),
+                //     ),
+                //     Container(
+                //       color: Colors.red,
+                //       height: 200,
+                //     ),
+                //     Container(
+                //       color: Colors.red,
+                //       height: 200,
+                //     )
+                //   ],
+                // ),
+
+                CarouselSlider(
+                  options: CarouselOptions(
+                    height: height * .2,
+                    aspectRatio: 16 / 9,
+                    viewportFraction: 0.8,
+                    initialPage: 0,
+                    enableInfiniteScroll: true,
+                    reverse: false,
+                    autoPlay: true,
+                    autoPlayInterval: Duration(seconds: 3),
+                    autoPlayAnimationDuration: Duration(milliseconds: 800),
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enlargeCenterPage: true,
+                    onPageChanged: (a, b) => print("asd"),
+                    scrollDirection: Axis.horizontal,
+                  ),
+                  items: [1, 2, 3, 4, 5].map((i) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: EdgeInsets.symmetric(horizontal: 5.0),
+                            decoration: BoxDecoration(color: Colors.amber),
+                            child: Text(
+                              'text $i',
+                              style: TextStyle(fontSize: 16.0),
+                            ));
+                      },
+                    );
+                  }).toList(),
+                ),
+
+                Container(
+                  color: Colors.green,
+                  child: Center(
+                    child: Text("Others"),
+                  ),
+                )
+              ],
+            )),
+          );
   }
 
   getToken() async {
